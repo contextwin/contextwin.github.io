5e245061ad8e7b5fed6cdd993da07e8fb9ad1bb3
contextwin <contextwin@example.com>
2020-04-20 16:14:23 +0900

    2020年  4月 20日 月曜日 16:14:23 JST

---
+++ projectdocs/image/change_exif.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+jhead -autorot $1
+
+#below easy explanation web pages url of this tool.
+#http://contextwin.livedoor.blog/archives/5808849.html
+
---
+++ projectdocs/image/image_resize.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+convert -geometry "$1%" $2 $3
+
+#below easy explanation web pages url of this tool.
+#http://contextwin.livedoor.blog/archives/5808849.html
