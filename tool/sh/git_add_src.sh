f7bd6a4b2dc4c4a3559fc3d3f7a6d653cb26ef23
contexwin <contextwin@gmail.com>
2021-07-02 09:37:01 +0900

    test

--- tool/sh/git_add_src.sh
+++ tool/sh/git_add_src.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DATE=`date`
-/usr/bin/git add index.html image projectdocs archives webautoarchive.sh url_list.txt git_add_src.sh tool
-/usr/bin/git commit -m "${DATE}"
-/usr/bin/git push origin master
-./webautoarchive.sh
+git add index.html image projectdocs archives webautoarchive.sh url_list.txt git_add_src.sh tool
+git commit -m "${DATE}"
+git push origin master
+#./webautoarchive.sh
